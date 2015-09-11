28a0e3993b114b42189b0b6609a98f7fcc047b63
codtatochip <codtatochip@users.noreply.github.com>
2015-09-11 12:22:57 +0600

    Complying to code standards

--- index.js
+++ index.js
@@ -1,122 +1,111 @@
-"use strict";
-var https = require('https');
-var fs = require('fs');
-var jsonPath = require('JSONPath');
-var download = require('download');
-var util = require('util');
+'use strict'
+var https = require('https')
+var jsonPath = require('JSONPath')
+var Download = require('download')
 
-function Scraper(){
-
+function Scraper () {
   // Only this one function is exposed
-  Scraper.prototype.scrap = function scrap(word, location, lang, name, callback){
-
+  Scraper.prototype.scrap = function scrap (word, location, lang, name, callback) {
     // Well, it is better to have something to look for
-    if(!word) throw new Error('Word must be defined');
-    if(!location) throw new Error('Word location be defined');
+    if (!word) throw new Error('Word must be defined')
+    if (!location) throw new Error('Word location be defined')
 
-    lang = lang || "en";
+    lang = lang || 'en'
 
     // Search, locate, retrieve
     // Error management at each stage
-    search(word, lang, function(err, fileName){
-      if(err) callback(err, null);
-      else {
-        locate(fileName, lang, function(err, url){
-          if(err) callback(err, null);
-          else {
-            retrieve(url, lang, location, name, callback);
+    search(word, lang, function (err, fileName) {
+      if (err) { callback(err, null) } else {
+        locate(fileName, lang, function (err, url) {
+          if (err) { callback(err, null) } else {
+            retrieve(url, lang, location, name, callback)
           }
-        });
+        })
       }
-    });
+    })
   }
 
-// Search: identify the name of first audio file on wiki page corresponding to the word
-function search(word, lang, callback){
+  // Search: identify the name of first audio file on wiki page corresponding to the word
+  function search (word, lang, callback) {
+    // Build the query - media files are "images". See https://www.mediawiki.org/wiki/API:Main_page
+    var url = 'https://' + lang + '.wiktionary.org/w/api.php?action=query&prop=images&format=json&iwurl=l&rawcontinue=&titles=' + word
 
-  // Build the query - media files are "images". See https://www.mediawiki.org/wiki/API:Main_page
-  var url = "https://" + lang + ".wiktionary.org/w/api.php?action=query&prop=images&format=json&iwurl=l&rawcontinue=&titles=" + word;
+    // Get the data
+    https.get(url, function (res) {
+      var data = ''
 
-  // Get the data
-  https.get(url, function(res) {
-    var data = "";
+      // Assemble data as it comes
+      res.on('data', function (chunk) {
+        data += chunk
+      })
 
-    // Assemble data as it comes
-    res.on('data', function(chunk) {
-      data += chunk;
-    });
-
-    // Once we got all the data, parse it
-    res.on('end', function() {
-      // Get the list of media files in the page
-      var images = jsonPath.eval(JSON.parse(data), "query.pages.*.images.*.title");
-      // Look for a sound (.ogg or .ogv) - stop at the first one
-      var i = 0;
-      var found = false;
-      var fileName = null;
-      while(i < images.length && !found){
-        fileName = new String(images[i]);
-        // We found one
-        if (fileName.indexOf('.ogg') != -1 || fileName.indexOf('.ogv') != -1){
-          found = true;
+      // Once we got all the data, parse it
+      res.on('end', function () {
+        // Get the list of media files in the page
+        var images = jsonPath.eval(JSON.parse(data), 'query.pages.*.images.*.title')
+        // Look for a sound (.ogg or .ogv) - stop at the first one
+        var i = 0
+        var found = false
+        var fileName = null
+        while (i < images.length && !found) {
+          fileName = images[i] + ''
+          // We found one
+          if (fileName.indexOf('.ogg') !== -1 || fileName.indexOf('.ogv') !== -1) {
+            found = true
+          }
+          i++
         }
-        i++;
-      }
-      var err = null;
-      if (!found){
-        fileName = null;
-        err = new Error("Can't find a page corresponding to the word");
-      }
-      callback(err, fileName);
-    });
+        var err = null
+        if (!found) {
+          fileName = null
+          err = new Error("Can't find a page corresponding to the word")
+        }
+        callback(err, fileName)
+      })
+    }).on('error', function (err) {
+      callback(err, null)
+    })
+  }
 
-  }).on('error', function(err) {
-    callback(err, null);
-  });
-}
+  // Locate: discover the url of the file based on its name
+  function locate (file, lang, callback) {
+    // Build the query - media files are "images". See https://www.mediawiki.org/wiki/API:Main_page
+    var url = 'https://' + lang + '.wiktionary.org/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&iwurl=l&rawcontinue=&titles=' + file
 
-// Locate: discover the url of the file based on its name
-function locate(file, lang, callback){
+    https.get(url, function (res) {
+      var data = ''
 
-  // Build the query - media files are "images". See https://www.mediawiki.org/wiki/API:Main_page
-  var url = "https://" + lang + ".wiktionary.org/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&iwurl=l&rawcontinue=&titles=" + file;
+      res.on('data', function (chunk) {
+        data += chunk
+      })
 
-  https.get(url, function(res) {
+      res.on('end', function () {
+        // JSON e.g.: https://en.wiktionary.org/w/api.php?action=query&prop=images&format=json&titles=shoe
+        var fileUrl = jsonPath.eval(JSON.parse(data), 'query.pages[-1].imageinfo..url')
+        var err = null
+        if (!fileUrl) {
+          err = new new Error("Can't locate the file")
+          fileUrl = null
+        } else {
+          fileUrl = fileUrl.toString()
+        }
+        callback(err, fileUrl)
+      })
+    }).on('error', function (err) {
+      callback(err, null)
+    })
+  }
 
-    var data = "";
-
-    res.on('data', function(chunk) {
-      data += chunk;
-    });
-
-    res.on('end', function() {
-      // JSON e.g.: https://en.wiktionary.org/w/api.php?action=query&prop=images&format=json&titles=shoe
-      var fileUrl = jsonPath.eval(JSON.parse(data), "query.pages[-1].imageinfo..url");
-      var err = null;
-      if (!fileUrl){
-        err = new new Error("Can't locate the file");
-        fileUrl = null;
-      }else {
-        fileUrl = fileUrl.toString();
-      }
-      callback(err, fileUrl);
-    });
-
-  }).on('error', function(err) {
-    callback(err, null);
-  });
-}
-
-// Retrieve: actually grab and download the audio file
-function retrieve(url, lang, location, name, callback){
-    new download()
-    .get(url)
-    .dest(location)
-    .rename(name)
-    .run(callback);
+  // Retrieve: actually grab and download the audio file
+  function retrieve (url, lang, location, name, callback) {
+    new Download()
+      .get(url)
+      .dest(location)
+      .rename(name)
+      .run(callback)
   }
 }
 
 // No need to create an instance outside the module
-var scraper = new Scraper();
-module.exports = scraper;
+var scraper = new Scraper()
+module.exports = scraper
--- test/test.js
+++ test/test.js
@@ -1,37 +1,31 @@
-"use strict";
+'use strict'
 
-var expect = require('chai').expect;
-var fs = require('fs');
-var scraper = require('../');
-var util = require('util');
-var path = require('path');
+var expect = require('chai').expect
+var fs = require('fs')
+var scraper = require('../')
+var path = require('path')
 
 // TODO Mock to avoid using network and file "En-us-shoe.ogg"
-describe("Scraper", function() {
-
-  this.timeout(5000);
-
-  it("Should scrap a file", function(done) {
-    scraper.scrap('shoe', __dirname, 'en', '',function(err, data){
-      var fn = function () {fs.readFileSync(path.resolve(__dirname, 'En-us-shoe.ogg'), null);}
-      expect(fn).to.not.throw(err);
-      done();
-    });
-  });
-
-  it("Should scrap a file and rename it", function(done) {
-    scraper.scrap('shoe', __dirname, 'en', 'file.ogg', function(err, data){
-      var fn = function () {fs.readFileSync(path.resolve(__dirname, 'file.ogg'), null);}
-      expect(fn).to.not.throw(err);
-      done();
-    });
-  });
-
-  it("Should fail and a return proper error (file does not exist)", function(done) {
-    scraper.scrap('Word that does not exist in Wiktionary', __dirname, 'en', '', function(err, data){
-      expect(err).to.be.an('Error');
-      done();
-    });
-  });
-
-});
+describe('Scraper', function () {
+  this.timeout(5000)
+  it('Should scrap a file', function (done) {
+    scraper.scrap('shoe', __dirname, 'en', '', function (err, data) {
+      var fn = function () { fs.readFileSync(path.resolve(__dirname, 'En-us-shoe.ogg'), null) }
+      expect(fn).to.not.throw(err)
+      done()
+    })
+  })
+  it('Should scrap a file and rename it', function (done) {
+    scraper.scrap('shoe', __dirname, 'en', 'file.ogg', function (err, data) {
+      var fn = function () { fs.readFileSync(path.resolve(__dirname, 'file.ogg'), null) }
+      expect(fn).to.not.throw(err)
+      done()
+    })
+  })
+  it('Should fail and a return proper error (file does not exist)', function (done) {
+    scraper.scrap('Word that does not exist in Wiktionary', __dirname, 'en', '', function (err, data) {
+      expect(err).to.be.an('Error')
+      done()
+    })
+  })
+})
