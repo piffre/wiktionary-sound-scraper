8b94c262f6d7c1f63bec19fa39c8fa0a11485f3a
piffre <piffre@users.noreply.github.com>
2015-09-18 10:55:30 +0600

    Added file format converter

--- index.js
+++ index.js
@@ -6,43 +6,59 @@ var urlencode = require('urlencode')
 var downloader = require('./lib/Downloader')
 var locator = require('./lib/Locator')
 var searcher = require('./lib/Searcher')
+var converter = require('./lib/Converter')
 
-function Scraper () {
-  // TODO: use opt obj {location:'__dirname', lang:'en', name:'', ext:'mp3'}
-  // TODO: allow word to be an array
-  Scraper.prototype.scrap = function scrap (word, location, lang, name, callback) {
-    if (!word || !location) {
-      callback(new Error('Word and location be defined'))
-      return
-    }
-    if (lang === undefined || name === undefined || callback === undefined) {
-      callback(new Error('Parameters are necessary, use empty strings'))
-      return
-    }
-    word = urlencode(word)
-    lang = lang || 'en'
-    async.waterfall([
-      function (cbk) {
-        searcher.search(word, lang, function (err, fileName) {
-          if (err) return cbk(err, null)
-          else cbk(null, fileName)
-        })
-      },
-      function (fileName, cbk) {
-        locator.locate(fileName, lang, function (err, url) {
-          if (err) return cbk(err, null)
-          else cbk(null, url)
-        })
-      },
-      function (url, cbk) {
-        downloader.download(url, location, name, function (err, data) {
-          cbk(err, data)
-        })
-      }
-    ], function (err, data) {
-      callback(err, data)
-    })
+
+function Scraper () {}
+
+// var opts = {location:folder, lang: 'fr', basename: 'new', ext: '.mp3'}
+// TODO: allow 'word' to be an array
+Scraper.prototype.scrap = function scrap (word, opts, callback) {
+  // Args check
+  if (arguments.length !== 3) {
+    return callback(new Error('Wrong number of arguments'), null)
   }
+  if (typeof (word) !== 'string' ||
+      typeof (opts) !== 'object' ||
+      typeof (callback) !== 'function') {
+    return callback(new Error('Wrong argument type'), null)
+  }
+
+  word = urlencode(word)
+  var location = opts.location ? opts.location : __dirname
+  var lang = opts.lang ? opts.lang : 'en'
+  var basename = opts.basename ? opts.basename : null
+  var ext = opts.ext ? opts.ext : null
+
+  async.waterfall([
+    function (cbk) {
+      searcher.search(word, lang, function (err, fileName) {
+        if (err) return cbk(err, null)
+        else cbk(null, fileName)
+      })
+    },
+    function (fileName, cbk) {
+      locator.locate(fileName, lang, function (err, url) {
+        if (err) return cbk(err, null)
+        else cbk(null, url)
+      })
+    },
+    function (url, cbk) {
+      downloader.download(url, location, basename, function (err, vinyl) {
+        if (err) return cbk(err, null)
+        else cbk(err, vinyl)
+      })
+    },
+    function (vinyl, cbk) {
+      converter.convert(vinyl.path, ext, function (err, data) {
+        if (err) return cbk(err, null)
+        else cbk(err, data)
+      })
+    }
+  ], function (err, data) {
+    callback(err, data)
+  })
 }
+
 var scraper = new Scraper()
 module.exports = scraper
--- test/test.js
+++ test/test.js
@@ -8,11 +8,10 @@ var expect = require('chai').expect
 var scraper = require('../')
 
 // TODO Mock to avoid using network and file "En-us-shoe.ogg"
+// TODO Delete files after tests
+
 describe('Scraper', function () {
   this.timeout(50000)
-
-  // TODO: use opt obj {location:'__dirname', lang:'en', name:'', ext:'mp3'}
-  var opts = {location:__dirname, lang: 'en', name: '', ext: '.mp3'}
 
   it('Should fail because of missing word parameter', function (done) {
     var opts = {}
@@ -25,7 +24,7 @@ describe('Scraper', function () {
   it('Should scrap a file (en)', function (done) {
     var opts = {}
     scraper.scrap('shoe', opts, function (err, data) {
-      var fn = function () { fs.readFileSync(path.resolve(__dirname, 'En-us-shoe.ogg'), null) }
+      var fn = function () { fs.readFileSync(path.resolve('En-us-shoe.ogg'), null) }
       expect(fn).to.not.throw(err)
       done()
     })
@@ -34,16 +33,16 @@ describe('Scraper', function () {
   it('Should scrap a file (ru)', function (done) {
     var opts = {lang: 'ru'}
     scraper.scrap('слон', opts, function (err, data) {
-      var fn = function () { fs.readFileSync(path.resolve(__dirname, 'Ru-слон.ogg'), null) }
+      var fn = function () { fs.readFileSync(path.resolve('Ru-слон.ogg'), null) }
       expect(fn).to.not.throw(err)
       done()
     })
   })
 
   it('Should scrap a file and rename it', function (done) {
-    var opts = {name: 'file'}
+    var opts = {basename: 'file'}
     scraper.scrap('shoe', opts, function (err, data) {
-      var fn = function () { fs.readFileSync(path.resolve(__dirname, 'file.ogg'), null) }
+      var fn = function () { fs.readFileSync(path.resolve('file.ogg'), null) }
       expect(fn).to.not.throw(err)
       done()
     })
@@ -62,14 +61,14 @@ describe('Scraper', function () {
   it('Should scrap a file and convert it', function (done) {
     var opts = {ext: '.mp3'}
     scraper.scrap('shoe', opts, function (err, data) {
-      var fn = function () { fs.readFileSync(path.resolve(__dirname, 'En-us-shoe.mp3'), null) }
+      var fn = function () { fs.readFileSync(path.resolve('En-us-shoe.mp3'), null) }
       expect(fn).to.not.throw(err)
       done()
     })
   })
 
   it('Should fail and a return proper error (word does not exist)', function (done) {
-    scraper.scrap('èèèe@@@%*',{}, function (err, data) {
+    scraper.scrap('èèèe@@@%*', {}, function (err, data) {
       expect(err).to.be.an('Error')
       done()
     })
@@ -77,15 +76,11 @@ describe('Scraper', function () {
 
   it('Should scrap a file, move it to another folder, rename it, and convert it (fr)', function (done) {
     var folder = __dirname + '/downloads/'
-    var opts = {location:folder, lang: 'fr', name: 'joujou', ext: '.mp3'}
+    var opts = {location: folder, lang: 'fr', basename: 'joujou', ext: '.mp3'}
     scraper.scrap('jouet', opts, function (err, data) {
       var fn = function () { fs.readFileSync(path.resolve(folder, 'joujou.mp3'), null) }
       expect(fn).to.not.throw(err)
       done()
     })
   })
-
-
-
-
 })
